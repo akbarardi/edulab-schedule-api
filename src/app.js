9ffad21d54169cd9206ccc197e3ccd3f97fea064
akbar_ardiansyah <akbarban27@gmail.com>
2026-06-11 16:55:40 +0700

    feat: add vercel deployment config

--- src/app.js
+++ src/app.js
@@ -5,9 +5,22 @@ require('dotenv').config();
 
 app.use(express.json());
 
+let dbReady = false;
+app.use(async (_req, _res, next) => {
+    if (!dbReady) {
+        await sequelize.sync();
+        dbReady = true;
+    }
+    next();
+});
+
 const scheduleRoutes = require('./routes/schedule.route');
 
 app.use('/api/schedules', scheduleRoutes);
 app.use('/public', express.static(path.join(process.cwd(), 'public')));
+app.get('/', (_req, res) => {
+    res.json({ status: 'ok', message: 'Edulab Schedule API' });
+});
+
 
 module.exports = app;
